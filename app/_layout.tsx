8a9badfeead99c355fcb37cfad29462030dbcf95
Freddy Tomada <freddytomada7@gmail.com>
2026-05-26 20:32:24 -0400

    add homeScreen and routing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import { useColorScheme } from "@/hooks/use-color-scheme";
 import {
   DarkTheme,
   DefaultTheme,
@@ -5,21 +6,44 @@ import {
 } from "@react-navigation/native";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/use-color-scheme";
-
-import { Text, View } from "react-native";
+import { allRoutes } from "@/constants/Routes";
+import { useThemeColor } from "@/hooks/use-theme-color";
+import { Stack } from "expo-router";
 import "../global.css";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const backgroundColor = useThemeColor({}, "background");
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <View className="bg-light-background dark:bg-dark-background">
-        <Text className="text-3xl mt-10 text-light-text dark:text-dark-text">
-          Hola Mundo
-        </Text>
-      </View>
+      <Stack
+        screenOptions={{
+          headerShadowVisible: false,
+          contentStyle: {
+            backgroundColor: backgroundColor,
+          },
+          headerStyle: {
+            backgroundColor: backgroundColor,
+          },
+        }}
+      >
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "",
+          }}
+        />
+        {allRoutes.map((route) => (
+          <Stack.Screen
+            key={route.name}
+            name={route.name}
+            options={{
+              title: route.title,
+            }}
+          />
+        ))}
+      </Stack>
     </ThemeProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     "./App.tsx",
     "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
+    "./presentation/**/*.{js,jsx,ts,tsx}",
   ],
   presets: [require("nativewind/preset")],
   theme: {
